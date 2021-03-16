9d6d343650fedc39e5727feeca3a22d8052ae86e
Erlangga15 <55235710+Erlangga15@users.noreply.github.com>
2021-03-16 15:29:57 +0700

    set the sb-admin as needed

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,18 @@
 module.exports = {
+  //Control views on admin 
   viewDashboard: (req, res) => {
     res.render('admin/dashboard/view_dashboard');
+  },
+  viewCategory: (req, res) => {
+    res.render('admin/category/view_category');
+  },
+  viewItem: (req, res) => {
+    res.render('admin/item/view_item');
+  },
+  viewBank: (req, res) => {
+    res.render('admin/bank/view_bank');
+  },
+  viewBooking: (req, res) => {
+    res.render('admin/booking/view_booking');
   },
 };
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,11 @@ const express = require('express');
 const route = express.Router();
 const adminController = require('../controllers/adminController');
 
+//? Route get from admin controllers
 route.get('/dashboard', adminController.viewDashboard);
+route.get('/category', adminController.viewCategory);
+route.get('/item', adminController.viewItem);
+route.get('/bank', adminController.viewBank);
+route.get('/booking', adminController.viewBooking);
 
 module.exports = route;
